710102779e1fe69d6f2928af3f0f5363775f1348
allyhere <camilomichelettors@gmail.com>
2019-05-15 07:15:16 -0300

    criar event listener para eventos de clique

--- scripts/controller/calcController.js
+++ scripts/controller/calcController.js
@@ -8,16 +8,32 @@ class CalcController {
         this._timeEl        = document.querySelector('#hora');// O underline no atributo significa que ele é privado
         this._actualDate; //this faz referencia ao objeto que foi instanciado a classe, ou seja o *calculadora*
         this.initialize();
+        this.initButtonEvents();
     }
 
     initialize(){
-        this.setDisplayDateTime();
+        this.setDisplayDateTime();//chama o método criado pra que a hora apareça à partir do 1º segundo
 
         setInterval(() =>{
-            this.displayDate = this.actualDate.toLocaleDateString(this._locale);
+            this.displayDate = this.actualDate.toLocaleDateString(this._locale,{//pra mostrar a hora por extenso
+                day:"2-digit",
+                month:"long",
+                year:"numeric"
+            });
             this.displayTime = this.actualDate.toLocaleTimeString(this._locale);
         }, 1000);
     }
+
+    initButtonEvents(){
+       let buttons = document.querySelectorAll("#buttons > g, #parts > g");//add event listener n se aplica em uma nodelist, tem que iterar a lista com ele
+       buttons.forEach((btn, index)=>{
+           btn.addEventListener("click", e=>{
+               console.log(btn.className.baseVal.replace("btn-",""))
+        })
+       })
+    }
+
+
     setDisplayDateTime(){
         this.displayDate = this.actualDate.toLocaleDateString(this._locale);
         this.displayTime = this.actualDate.toLocaleTimeString(this._locale);
